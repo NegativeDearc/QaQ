ae7bc24aefe4e08df02eafcf4a4c30b0e9a2da7d
Dearc <NegativeDearc@users.noreply.github.com>
2018-02-05 15:28:34 +0800

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo on
+cd c:\Card_Practise\
+c:\Card_Practise\venv\Scripts\python.exe run.py
+@pause
---
+++ run.vbs
@@ -0,0 +1,2 @@
+set ws=WScript.CreateObject("WScript.Shell") 
+ws.Run "c:\Card_Practise\run.bat",0
